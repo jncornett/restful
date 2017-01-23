ce96b756a132fb809500f5952469641ee7414e26
Joel Cornett <joel.cornett@gmail.com>
2017-01-22 23:45:59 -0500

    add example usage of json client

---
+++ client_test.go
@@ -0,0 +1,27 @@
+package restful_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jncornett/restful"
+)
+
+// Example usage of the default JSON client
+func ExampleClient_JSONClient() {
+	client := restful.NewJSONClient(
+		"http://www.example.com/api",
+		// Default constructor for an anonymous struct
+		func() interface{} { return &struct{ First, Last string }{} },
+		// Default constructor for an anonymous slice list
+		func() interface{} { return &[]struct{ First, Last string }{} },
+	)
+	// lookup all records
+	list, err := client.GetAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, item := range list.([]struct{ First, Last string }) {
+		fmt.Println(item)
+	}
+}
